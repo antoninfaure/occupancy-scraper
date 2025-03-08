4141506ee660c8af5d9abce630efce934c90425a
antoninfaure <antonin.faure@epfl.ch>
2025-03-08 10:57:08 +0100

    update date

--- update_courses.py
+++ update_courses.py
@@ -31,39 +31,39 @@ utils.add_teachers_to_courses(db, unique_courses)
 # Create semesters
 
 # Fall 2024-2025
-skip_dates = [
-    datetime(2024, 9, 16),
-    datetime(2024, 10, 21),
-    datetime(2024, 10, 22),
-    datetime(2024, 10, 23),
-    datetime(2024, 10, 24),
-    datetime(2024, 10, 25),
-    datetime(2025, 4, 18),
-    datetime(2025, 4, 21),
-    datetime(2025, 4, 22),
-    datetime(2025, 4, 23),
-    datetime(2025, 4, 24),
-    datetime(2025, 4, 25),
-    datetime(2025, 5, 29),
-]
+# skip_dates = [
+#     datetime(2024, 9, 16),
+#     datetime(2024, 10, 21),
+#     datetime(2024, 10, 22),
+#     datetime(2024, 10, 23),
+#     datetime(2024, 10, 24),
+#     datetime(2024, 10, 25),
+#     datetime(2025, 4, 18),
+#     datetime(2025, 4, 21),
+#     datetime(2025, 4, 22),
+#     datetime(2025, 4, 23),
+#     datetime(2025, 4, 24),
+#     datetime(2025, 4, 25),
+#     datetime(2025, 5, 29),
+# ]
 
-utils.create_new_semester(db,
-    name="Semestre d'automne 2024-2025",
-    start_date=datetime(2024, 9, 9),
-    end_date=datetime(2024, 12, 22),
-    skip_dates=skip_dates,
-    type="fall",
-    available=True
-)
+# utils.create_new_semester(db,
+#     name="Semestre d'automne 2024-2025",
+#     start_date=datetime(2024, 9, 9),
+#     end_date=datetime(2024, 12, 22),
+#     skip_dates=skip_dates,
+#     type="fall",
+#     available=True
+# )
 
-utils.create_new_semester(db,
-    name="2024-2025",
-    start_date=datetime(2024, 9, 9),
-    end_date=datetime(2025, 6, 1),
-    skip_dates=skip_dates,
-    type="year",
-    available=True
-)
+# utils.create_new_semester(db,
+#     name="2024-2025",
+#     start_date=datetime(2024, 9, 9),
+#     end_date=datetime(2025, 6, 1),
+#     skip_dates=skip_dates,
+#     type="year",
+#     available=True
+# )
 
 # Create units
 utils.create_units(db, unique_courses)
